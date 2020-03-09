a73a0794d77cfec369102e55541baa85e77123f7
inegg <inegg.apps@gmail.com>
2020-03-09 12:37:21 +0900

    테이블에 관한 기본적인 정보 확인하는 구문

---
+++ 02-1-기본요소/20200309-01-테이블정보-hr.sql
@@ -0,0 +1,9 @@
+ --존재하는 테이블 조회
+select * from user_tables;
+select * from tab;
+
+-- 딕셔너리
+-- 테이블의 칼럼명 조회
+select * from USER_TAB_COLUMNS;
+select * from COLS;
+select * from COLS where table_name='JOBS';
---
+++ 02-1-기본요소/20200309-01-테이블정보-관리자.sql
@@ -0,0 +1,3 @@
+select * from ALL_TABLES;
+select * from ALL_TABLES where owner ='HR';
+select * from ALL_TABLES where owner='SKY';
